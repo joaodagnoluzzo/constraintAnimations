b56d08760aa231c670363fff98d94fd6d1ec3a16
João Pedro Cappelletto D'Agnoluzzo <joaop@dbserver.com.br>
2020-02-21 12:37:00 -0300

    Initial Commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by dbserver on 21/02/20.
+//  Copyright © 2020 dbserver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
